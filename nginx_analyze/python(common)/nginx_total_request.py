5db568b8167347985752fda60b441def312654cd
stefanmonkey <stefan_bo@163.com>
2014-03-18 14:21:09 +0800

    add check print

--- nginx_analyze/python(common)/nginx_total_request.py
+++ nginx_analyze/python(common)/nginx_total_request.py
@@ -51,8 +51,9 @@ results = 0
 for file in file_list:
     index = open(os.path.join(nginx_etc_path, file), 'r').read().split('\n')
     access_log = conf_list_ana('access_log', index)[1].replace(';', '')
-    
+    #print os.path.join(nginx_base, access_log)
     ab = log_analyze(os.path.join(nginx_base, access_log))
+    #print ab
     results += ab
     
 print results
